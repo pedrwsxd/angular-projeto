aea7335fc79752186d9755639d3648d4aaf97e96
Pedro_Henrick <Pedrwsxd@gmail.com>
2024-10-15 16:46:20 -0300

    Atualização 15.10.24

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  exibirIconeCarrinho: boolean = false;
+  faShoppingCart = faShoppingCart;
 
-  constructor(private router: Router){
-
+  constructor(private router: Router) {
+   
+    this.router.events.subscribe(() => {
+      this.exibirIconeCarrinho = this.router.url === '/cardapio';
+    });
   }
-  goToCardapio(){
-window.location.href="/cardapio"
+  goToCardapio() {
+    window.location.href = '/cardapio';
   }
 }
